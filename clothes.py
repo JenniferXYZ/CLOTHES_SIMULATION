d3547f8b72de52cdbd2d99efe868e26b35f5f136
JenniferZhang <jenniferzhang1997@gmail.com>
2018-07-13 01:20:30 -0400

    Add double_sided feature

--- clothes.py
+++ clothes.py
@@ -37,30 +37,52 @@ def import_obj () :
 	cmds.select('Object1',r = True)
 	cmds.polyReduce(ver = 1, p = 60.0)
 
+'''buggy'''
+def double_sided() :
+	cmds.select('Object1',r = True)
+	dup = cmds.duplicate('Object1')
+	cmds.rename('Object1_dup')
+	cmds.select('Object1_dup',r = True)
+	cmds.polyNormal(normalMode = 0, ch = True)
+	#Now we have two objects, Object1 and Object1_dup
+	#Needs to assign texture to both
+
+
+#Object_List is a list of strings
 def modify_tex () :
+
 	#create and assign texture
 	textureA = cmds.shadingNode("lambert", asShader = True, name = "TextureA")
 	cmds.setAttr(textureA + ".color", 0.5,0.78,0.28)#grey
 	textureASG = cmds.sets( renderable = True, noSurfaceShader = True, empty = True, name = 'TextureASG')
 
-	cmds.select('Object1',r = True)
-	cmds.defaultNavigation(connectToExisting = True, source = "TextureA", destination = "TextureASG")
-	cmds.sets(e = True, forceElement = "TextureASG")
+	#get all objects in scene
+	cmds.select(ado = True)
+	Object_List = cmds.ls(selection = True)
+
+	for item in Object_List :
+		cmds.select(item,r = True)
+		cmds.defaultNavigation(connectToExisting = True, source = "TextureA", destination = "TextureASG")
+		cmds.sets(e = True, forceElement = "TextureASG")
 
 
 def export_fbx() :
-	cmds.select('Object1',r = True)
+	cmds.select(ado = True)
 	cmds.file(wrspFldr + '//' + filename + '.fbx', f = True, op = "v = 0;", typ = "FBX export", pr = True, es = True)
 
 
 
 # MAIN FUNCTION
 def main():
-	get_working_dir()
-	import_obj()
-	modify_tex()
-	export_fbx()
+	
+	#get_working_dir()
+	#import_obj()
+	# double_sided()
 
+
+	# modify_tex()
+	export_fbx()
+	print "Here"
 
 
 if __name__ == '__main__':
--- import_ext_script.py
+++ import_ext_script.py
@@ -1,3 +1,4 @@
+import maya.cmds as cmds
 import sys
 import os
 
