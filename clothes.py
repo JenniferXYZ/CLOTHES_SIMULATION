1f394b6811d66bd7170a0a16d6813e324cfe549f
JenniferZhang <jenniferzhang1997@gmail.com>
2018-07-26 22:48:22 -0400

    added reading json code to clothes.py

--- clothes.py
+++ clothes.py
@@ -1,10 +1,9 @@
 import maya.cmds as cmds
 import os.path
 import sys
+import json
 
 
-#Global Variable
-filename = "test"
 
 def get_working_dir () :
 	global wrspFldr
@@ -14,18 +13,26 @@ def get_working_dir () :
 	if os.path.exists(sceneName):
 	   # current folder    
 	   wrspFldr = os.path.dirname(sceneName)
-	   print (wrspFldr + "yay")
+	   # print (wrspFldr + "yay")
 	else:
 	   print "Cannot find current project directory" 
 	 
+def load_json() :
+	json_data = open(wrspFldr + '/textureData.json')
+	# target to parse
+	data = json.load(json_data)
+	# number of items
+	item_num = len(data)
+	json_data.close()
 
 
-def import_obj () :
+
+def import_obj (fn,rel_path) :
 
 	#import obj files  
-	cmds.file( wrspFldr + '//Models/' + filename + '.obj', i = True, type = 'OBJ', iv = True, ra = True, ns = filename, mnc = True)
+	cmds.file( wrspFldr + '//Models/' + rel_path + fn + '.obj', i = True, type = 'OBJ', iv = True, ra = True, ns = fn, mnc = True)
 	
-	tempfn = filename + ":Mesh"
+	tempfn = fn + ":Mesh"
 
 	cmds.select(tempfn,r = True)
 
@@ -39,34 +46,25 @@ def import_obj () :
 
 
 #Object_List is a list of strings
-def modify_tex () :
+def modify_tex (fn,R,G,B) :
 
 	#create and assign texture
 	textureA = cmds.shadingNode("lambert", asShader = True, name = "TextureA")
-	cmds.setAttr(textureA + ".color", 0.5,0.78,0.28)#grey
+	cmds.setAttr(textureA + ".color", float(R/255),float(G/255),float(B/255))#grey
 	textureASG = cmds.sets( renderable = True, noSurfaceShader = True, empty = True, name = 'TextureASG')
 
-	#get all objects in scene
-	# cmds.select(ado = True)
-	
-	# global Object_List
-	# Object_List = cmds.ls(selection = True)
+
 	cmds.select(tempfn, r = True)	
 	cmds.defaultNavigation(connectToExisting = True, source = "TextureA", destination = "TextureASG")
 	cmds.sets(e = True, forceElement = "TextureASG")
 
-	# for item in Object_List :
-	# 	cmds.select(item,r = True)
-	# 	cmds.defaultNavigation(connectToExisting = True, source = "TextureA", destination = "TextureASG")
-	# 	cmds.sets(e = True, forceElement = "TextureASG")
 
-# def sewing_pieces() :
-# 	cmds.polyUnite(Object_List, n = 'result')
-
-# def export_fbx() :
+def export_fbx(fn) :
 # 	# cmds.select(ado = True)
 	cmds.select(tempfn, r = True)	
-	cmds.file(wrspFldr + '//' + filename + '.fbx', f = True, op = "v = 0;", typ = "FBX export", pr = True, es = True)
+
+	# need to choose a folder for output
+	cmds.file(wrspFldr + '//' + fn + '.fbx', f = True, op = "v = 0;", typ = "FBX export", pr = True, es = True)
 
 
 def cleanup(fn) :
@@ -81,13 +79,22 @@ def cleanup(fn) :
 # MAIN FUNCTION
 def main():
 	
-	# get_working_dir()
-	cleanup(filename)
-	# import_obj()
-	# double_sided()
-	modify_tex()
-	# sewing_pieces()
-	export_fbx()
+	get_working_dir()
+	load_json()
+
+	for i in range(0:item_num-1):
+		
+		filename = data[i]["name"]	#filename without extension
+		path = data[i]["pathfolder"] # reletive to wrspFldr
+		R = data[i]["r"]	# range: 0 - 255
+		G = data[i]["g"]
+		B = data[i]["b"]
+
+		cleanup(filename)
+		import_obj(filename)
+		modify_tex(filename,R,G,B)
+		export_fbx(filename)
+
 	print "Here"
 
 
--- read_json.py
+++ read_json.py
@@ -1,30 +1,52 @@
 import json
 import sys
 import os
+import maya.cmds as cmds
 
-
-
-
+def get_working_dir () :
+	global wrspFldr
+	#Get working directory and script location
+	sceneName = cmds.file( q = True, sn = True)
+	print (sceneName)
+	if os.path.exists(sceneName):
+	   # current folder    
+	   wrspFldr = os.path.dirname(sceneName)
+	   print (wrspFldr + "yay")
+	else:
+	   print "Cannot find current project directory" 
+	 
 
 def read_func() :
-	json_data = open('C:/Users/Jennifer Zhang/Documents/CLOTHES_SIMULATION/textureData.json')
+	json_data = open(wrspFldr + '/textureData.json')
 	data = json.load(json_data)
+	item_num = len(data)
+	# print (item_num)
 
-	print(data)
+	for i in range(0,item_num-1):
+		path = data[i]["pathfolder"]
+		R = data[i]["r"]
+		G = data[i]["g"]
+		B = data[i]["b"]
+		
 
-	
+
+	# print(data)
+	# print(data[0]["r"])
+
+	# print(data[0]["b"])
 
 
 	# target = json.loads("C:/Users/Jennifer Zhang/Documents/CLOTHES_SIMULATION/textureData.json")
 
 
-	print ("hi")
+	# print ("hi")
  
 	json_data.close()
 
 
 
 def main() :
+	get_working_dir()
 	read_func()
 
 if __name__ == '__main__':
